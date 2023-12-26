a293ca201170942b335dd05187e2b15c86b4deb7
MichaelObvious <michele.palese42@outlook.it>
2023-12-26 01:27:54 +0100

    improvements

--- hr.c
+++ hr.c
@@ -320,8 +320,8 @@ void hour_name(FILE* sink, int hour, int minute, int sec, int day, int month, in
     } else {
 		int vigilia; 
 		if (current_time < rise_time) {
-			vigilia = (current_time + 24 * 60 - prev_set_time) / prev_vigilia_duration;
-			*progress = (double)((current_time + 24 * 60 - prev_set_time) % prev_vigilia_duration) / (double) prev_vigilia_duration;
+			vigilia = (current_time + 24 * 3600 - prev_set_time) / prev_vigilia_duration;
+			*progress = (double)((current_time + 24 * 3600 - prev_set_time) % prev_vigilia_duration) / (double) prev_vigilia_duration;
 		} else {
 			vigilia = (current_time - set_time) / next_vigilia_duration;
 			*progress = (double)((current_time - set_time) % next_vigilia_duration) / (double) next_vigilia_duration;
@@ -529,9 +529,13 @@ int main(int argc, char** argv) {
 
         fflush(stdout);
 
-        usleep(500*1000);
+		if (watch)
+        	usleep(500*1000);
+		
     } while (watch);
+
     fprintf(stdout, "\n");
 
     return 0;
 }
+#endif
